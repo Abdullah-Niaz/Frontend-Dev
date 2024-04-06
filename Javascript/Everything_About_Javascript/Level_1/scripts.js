5d1706ed58da5932ab427188d21a3f728c92997e
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-04-07 01:52:11 +0500

    js load, console,prompt,alert

---
+++ Javascript/Everything_About_Javascript/Level_1/scripts.js
@@ -0,0 +1,13 @@
+// var x = 1
+// function data(){
+//     if(!x){
+//         var x = 20;
+//     }
+// console.log(x)
+// }
+// data();
+// console.log(x);
+
+
+var n = prompt("Enter your name","John Doe")
+console.log(n);
