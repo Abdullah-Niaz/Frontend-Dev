1edd1c5150e84ebfee4776f8031549c89bb4b70b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-04-30 22:50:00 +0500

    asynchronous

---
+++ Javascript/Asynchronous_JavaScript/code.js
@@ -0,0 +1,34 @@
+// settimeout - Runs after taking few time
+// it will run after 1S 
+// if there is any sync before and after the function 
+// they will run before the settimeout call
+// console.log("Hello World 1");
+// console.log("Hello World 2");
+// setTimeout(() => {
+//     console.log("Hello World");
+// }, 1000);
+// console.log("Hello World 3");
+// console.log("Hello World 4");
+
+
+// setInterval kuch der bad bar bar chly ga
+// Y given time k ander bar bar chalta rahy ga
+// var count = 1;
+// let hmarinter = setInterval(() => {
+//     console.log(count);
+//     ++count;
+//     if(count === 5){
+//         // hmarinter(setInterval);
+//         clearInterval(hmarinter);
+//     }
+// }, 1000);
+
+// we can stop the setINterval 
+// we can make a variale and can call the fucntion side that
+// another way is to break it inside the fucntion
+
+
+
+
+// Fetch API -- data ly kar use kar ga, ya pher de ga form urls
+ 
