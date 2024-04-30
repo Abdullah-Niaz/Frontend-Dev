9f9c1f5a52f2f0746ae2abbfdf7f584d48c08eea
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-04-30 23:42:06 +0500

    fetching data through api

--- Javascript/Asynchronous_JavaScript/code.js
+++ Javascript/Asynchronous_JavaScript/code.js
@@ -31,4 +31,38 @@
 
 
 // Fetch API -- data ly kar use kar ga, ya pher de ga form urls
- 
+// fetch('https://randomuser.me/api/')
+// .then(raw => raw.json())
+// .then(readData => console.log(readData.results[0]));
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(raw => raw.json())
+// .then(readable => console.log(readable.userID[1]))
+
+
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return response.json();
+    })
+    .then(posts => {
+        posts.forEach(post => {
+            console.log("Title:", post.title);
+            console.log("Body:", post.body);
+            console.log("User ID:", post.userId);
+            console.log("ID:", post.id);
+            console.log();
+            if(post.id == 3){
+                return 0;
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+    });
