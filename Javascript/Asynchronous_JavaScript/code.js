e8f3f0de478af79468005e99b1790a2f65d5cdb6
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-01 22:04:20 +0500

    Asychrnouos

--- Javascript/Asynchronous_JavaScript/code.js
+++ Javascript/Asynchronous_JavaScript/code.js
@@ -40,29 +40,54 @@
 // .then(raw => raw.json())
 // .then(readable => console.log(readable.userID[1]))
 
+// fetch("https://api.openweathermap.org/data/2.5/weather")
+// .then(raw => raw.json())
+// .then(readable => console.log(readable))
 
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+// fetch("https://api.github.com/repos/Abdullah-Niaz/Data-Structure-Algorithm")
+// .then(raw => raw.json())
+// .then(readable => console.log(readable))
 
-fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-        return response.json();
-    })
-    .then(posts => {
-        posts.forEach(post => {
-            console.log("Title:", post.title);
-            console.log("Body:", post.body);
-            console.log("User ID:", post.userId);
-            console.log("ID:", post.id);
-            console.log();
-            if(post.id == 3){
-                return 0;
-            }
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error.message);
-    });
+
+
+// const url = 'https://jsonplaceholder.typicode.com/posts';
+
+// fetch(url)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Failed to fetch data');
+//         }
+//         return response.json();
+//     })
+//     .then(posts => {
+//         posts.forEach(post => {
+//             console.log("Title:", post.title);
+//             console.log("Body:", post.body);
+//             console.log("User ID:", post.userId);
+//             console.log("ID:", post.id);
+//             console.log();
+//             if(post.id == 3){
+//                 return 0;
+//             }
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error:', error.message);
+//     });
+
+
+
+
+
+
+// Axious 
+// Axios are similar to the Fetch API But...
+// Axios is a promise based HTTP client for the browser and Node.js.
+// Axios provides a simple to use library in a small package with a very extensible interface.
+axios.get("https://randomuser.me/api/")
+.then((result) => {
+    console.log(result)   
+}).catch((err) => {
+    console.log("Error in Fetching")
+});
