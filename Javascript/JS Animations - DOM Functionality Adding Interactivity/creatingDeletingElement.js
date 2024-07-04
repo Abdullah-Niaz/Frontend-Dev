305770bd4294df8ed965f669a32df7cfaffd28de
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-04 12:34:36 +0500

    creating & deleting  elements in html using js

---
+++ Javascript/JS Animations - DOM Functionality Adding Interactivity/creatingDeletingElement.js
@@ -0,0 +1,10 @@
+var btn = document.createElement("button");
+btn.textContent = "Click me";
+document.querySelector("body").appendChild(btn)
+
+var hr = document.createElement("hr");
+document.querySelector("body").appendChild(hr)
+var img = document.createElement("img");
+img.src = "https://bityl.co/Qsg8"
+document.querySelector("body").appendChild(img)
+document.querySelector("body").removeChild(img)
