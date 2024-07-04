1964857f919686c9a5ee37cdf8f052414893a618
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-04 12:07:33 +0500

    ElementByNameSelector

--- Javascript/JS Animations - DOM Functionality Adding Interactivity/Understanding_Dom.js
+++ Javascript/JS Animations - DOM Functionality Adding Interactivity/Understanding_Dom.js
@@ -1,6 +1,19 @@
-var h = document.querySelector("h1");
-var x = ["Ali", "omer", "Talha", "Hamza"];
+// var h = document.querySelector("h1");
+// var x = ["Ali", "omer", "Talha", "Hamza"];
 
-for (let i = 0; i < x.length; i++) {
-    h.textContent += x[i] + " "; 
-}
+// for (let i = 0; i < x.length; i++) {
+//     h.textContent += x[i] + " "; 
+// }
+
+
+var btn = document.getElementsByName("button");
+
+for (let i = 0; i < btn.length; i++) {
+        btn[i].textContent = "Done...";
+    }
+    
+var btn = document.querySelector("button")
+btn.textContent  = "Done."
+
+// var btn = document.querySelector("button");
+// btn.innerHTML = "<i>Hello World</i>";
