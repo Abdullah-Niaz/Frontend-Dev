15747dff31006eaa96ac4b8429abaec59280f4ba
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-07 15:45:41 +0500

    GSAP To, From Animation

---
+++ GSAP/GSAP_intro/script.js
@@ -0,0 +1,16 @@
+// gsap.to("#box",{
+//     x: 450,
+//     y:200,
+//     rotate:360,
+//     backgroundColor:"green",
+//     duration:3,
+//     delay:1,
+// })
+gsap.from("#box",{
+    x: 450,
+    y:200,
+    rotate:360,
+    backgroundColor:"green",
+    duration:3,
+    delay:1,
+})
