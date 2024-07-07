133304e912afaa5625a7a5695cdb3b3624685db9
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-07 15:46:19 +0500

    GSAP time line to control both(to,from) at same time

---
+++ GSAP/GSAP_timeline/script.js
@@ -0,0 +1,26 @@
+var tl = gsap.timeline()
+
+
+tl.to("#box1",{
+    duration: 1,
+    x: 100,
+    ease: "power1.out",
+    delay: 1,
+
+})
+
+tl.to("#box1",{
+    duration: 1,
+    x: 100,
+    ease: "power1.out",
+    delay: 1,
+
+})
+
+tl.to("#box1",{
+    duration: 1,
+    x: 100,
+    ease: "power1.out",
+    delay: 1,
+
+})
