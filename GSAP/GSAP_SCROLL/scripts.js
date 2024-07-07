e3c3a521ee52bba0c3cbfb4460ada72c280fc875
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-07 23:40:49 +0500

    GSAP Scroll Trigger

---
+++ GSAP/GSAP_SCROLL/scripts.js
@@ -0,0 +1,22 @@
+
+gsap.from("#page1 #circle", {
+  scale: 0,
+  rotate: 360,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.from("#page2 #circle", {
+  scale: 0,
+  rotate: 360,
+  duration: 2,
+  delay: 1,
+});
+
+gsap.from("#page3 #circle", {
+  scale: 0,
+  rotate: 360,
+  duration: 2,
+  delay: 1,
+});
+
